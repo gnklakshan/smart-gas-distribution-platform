139b04e584398f368b3eda56b97c9b53372c9bb4
gnklakshan <132909147+gnklakshan@users.noreply.github.com>
2026-04-29 19:23:58 +0530

    add UserResponse

---
+++ user-service/src/main/java/com/gastracker/user_service/dto/response/UserResponse.java
@@ -0,0 +1,18 @@
+package com.gastracker.user_service.dto.response;
+
+import com.gastracker.user_service.enums.Role;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class UserResponse {
+    private String id;
+    private String nic;
+    private String email;
+    private String name;
+    private Role role;
+    private LocalDateTime createdAt;
+}
