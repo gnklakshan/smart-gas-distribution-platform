29fb550cc181cc92a0192b1a4dc33a01cc59f99c
gnklakshan <132909147+gnklakshan@users.noreply.github.com>
2026-04-29 19:23:34 +0530

    add User entity

---
+++ user-service/src/main/java/com/gastracker/user_service/dao/entity/User.java
@@ -0,0 +1,44 @@
+package com.gastracker.user_service.dao.entity;
+
+import com.gastracker.user_service.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(unique = true, nullable = false)
+    private String nic;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+}
